97f753fdc5038b3b32441ccc507c35329a6a12b5
biplabmon <biplab8629@gmail.com>
2024-02-14 22:54:01 +0530

    completed dark mode functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -27,6 +27,6 @@ const userSlice = createSlice({
     },
 });
 
-export const {signInStart, signInSuccess, signInFailure} = userSlice.actions;
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
 
 export default userSlice.reducer;
