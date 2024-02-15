01fdcef7872b6fb0cd7d831fd956f5945d9c053b
biplabmon <biplab8629@gmail.com>
2024-02-15 15:22:25 +0530

    complete sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Sidebar } from 'flowbite-react';
+import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+
+const DashSidebar = () => {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);    // it's JS native functionality to get the current tab location
+        const tabFromUrl = urlParams.get('tab');
+        console.log(tabFromUrl);
+
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
+
+    }, [location.search]);
+
+    return (
+        <Sidebar className='w-full md:w-56'>
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Link to='/dashboard?tab=profile'>
+                        <Sidebar.Item
+                            active={tab === 'profile'}
+                            icon={HiUser} label={'User'}
+                            labelColor='dark'
+                        >
+                            Profile
+                        </Sidebar.Item>
+                    </Link>
+                    <Sidebar.Item icon={HiArrowSmRight} className=''>
+                        Sign Out
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
+        </Sidebar>
+    )
+}
+
+export default DashSidebar
