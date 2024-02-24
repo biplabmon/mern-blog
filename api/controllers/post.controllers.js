0cf663ab36d1f39706ca62efb12a51e697f0792c
biplabmon <biplab8629@gmail.com>
2024-02-24 21:11:03 +0530

    create get posts API

--- api/controllers/post.controllers.js
+++ api/controllers/post.controllers.js
@@ -28,3 +28,50 @@ export const create = async (req, res, next) => {
         next(error);
     }
 };
+
+// get the all post
+export const getposts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === 'asc' ? 1 : -1;
+        const posts = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),        // if user search based on user id
+            ...(req.query.category && { category: req.query.category }),      // if category search based on category
+            ...(req.query.slug && { slug: req.query.slug }),      // if slug search based on slug or post title
+            ...(req.query.postId && { _id: req.query.postId }),       // if post id search based on post id
+
+            // if post title search based on post title or post subtitle or post content
+            ...(req.query.searchTerm && {
+                $or: [
+                    { title: { $regex: req.query.searchTerm, $options: 'i' } },
+                    { content: { $regex: req.query.searchTerm, $options: 'i' } },
+                ]
+            })
+        }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit)
+
+        const totalPost = await Post.countDocuments();
+
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+
+        const lastMonthPost = await Post.countDocuments({
+            createdAt: {
+                $gte: oneMonthAgo
+            }
+        });
+
+        res.status(200).json({
+            posts,
+            totalPost,
+            lastMonthPost
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
