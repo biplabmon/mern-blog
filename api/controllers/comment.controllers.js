4c4d7e2d2497137c4642c3d1959ac410f31b51e2
biplabmon <biplab8629@gmail.com>
2024-03-09 23:25:09 +0530

    add comment section to the post page

---
+++ api/controllers/comment.controllers.js
@@ -0,0 +1,25 @@
+import Comment from "../models/comment.model.js";
+import { errorHandler } from "../utils/error.js";
+
+
+// create a new comment
+export const createComment = async (req, res, next) => {
+    try {
+        const {content, userId, postId} = req.body;
+
+        if(userId !== req.user.id){
+            return next(errorHandler(403, 'You are not allowed to create a comment'))
+        };
+
+        const newComment = new Comment({
+            content,
+            userId,
+            postId
+        });
+        await newComment.save();
+        
+        res.status(201).json(newComment);
+    } catch (error) {
+        next(error);
+    }
+};
